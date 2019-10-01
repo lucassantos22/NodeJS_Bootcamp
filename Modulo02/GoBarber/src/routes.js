c066863b16046548fe360e28116e30764807af21
Lucas Santos <lucas.afsantos@hotmail.com>
2019-09-30 22:20:09 -0300

    Estudando sequelize

--- Modulo02/GoBarber/src/routes.js
+++ Modulo02/GoBarber/src/routes.js
@@ -1,8 +1,14 @@
 import {Router} from 'express';
 const routes = new Router();
+import User from './app/models/User';
 
-routes.get('/', (req,res)=>{
-    res.json({a:'a'})
+routes.get('/', async (req,res)=>{
+    const user = await User.create({
+        name: 'Diego',
+        email: 'Email',
+        password_hash: '123456'
+    })
+    res.json(user);
 })
 
 export default routes;
