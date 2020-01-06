043d97b6cc3dc64997e7bd5b09e9f50525c4eb50
Lucas Santos <lucas.afsantos@hotmail.com>
2020-01-06 01:32:30 -0200

    Montando front-end do projeto de treino pessoal.

--- modulo05/src/pages/repository/index.js
+++ modulo05/src/pages/repository/index.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Api from '../../services/api'
+import propTypes from 'prop-types'
 
-function Repository({match}){
-    return <h1>Repositório: {decodeURIComponent(match.params.repository)}</h1>
+export default class Repository extends Component {
+    static propTypes = {
+        match: propTypes.shape({
+           params: propTypes.shape({
+               repository: propTypes.string
+           })
+        }).isRequired,
+
+    };
+    async componentDidMount(){
+        const {match} = this.props;
+        const repoName = decodeURIComponent(match.params.repository);
+
+        const [repository, issues] = await Promise.all([
+            Api.get(`/repos/${repoName}`),
+            Api.get(`/repos/${repoName}/issues`, {
+                params:{
+                    state: 'open',
+                    per_page: 5
+                }
+            })
+        ]);
+        this.setState(
+            {loading:false,
+                repository: repository.data,
+                issues: issues.data
+            }
+        )
+    }
+    constructor(props){
+        super(props);
+        this.state = {
+            repository: {},
+            issues: [],
+            loading: true
+        }
+    }
+    render() {
+        const {repository, issues, loading} = this.state;
+        return <h1>Repositório</h1>
+    }
+
 }
-
-export default Repository;
